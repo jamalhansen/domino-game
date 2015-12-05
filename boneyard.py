c2d3e87b6ff9232cb1b442ef2efb0ed3f364d3a8
Jamal Hansen <jamal.hansen@gmail.com>
2015-12-04 22:38:54 -0600

    Allows in comparisons of dominoes with dunder eq()

--- boneyard.py
+++ boneyard.py
@@ -4,25 +4,12 @@ from random import shuffle
 def add_dominoes(dots):
     dominoes = []
     for dots1 in range(dots + 1 ):
-        
+
         for dots2 in range(dots + 1):
             new_domino = domino(dots1, dots2)
 
-            if check_for_domino(dominoes, new_domino):
+            if new_domino not in dominoes:
                 dominoes.append(new_domino)
 
     return dominoes
 
-def check_for_domino(dominoes, new_domino):
-    if dominoes:
-        for domino in dominoes:
-            if (new_domino.dots1, new_domino.dots2) == (domino.dots1, domino.dots2):
-                return False
-
-            elif (new_domino.dots1, new_domino.dots2) == (domino.dots2, domino.dots1):
-                return False
-            
-        return True
-    
-    else:
-        return True

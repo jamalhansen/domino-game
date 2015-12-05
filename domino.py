d9f71cf1b8249b53bc51b908292ca5dcca2b854d
Jamal Hansen <jamal.hansen@gmail.com>
2015-12-05 13:12:26 -0600

    Changes domino.dots back to a tuple

--- domino.py
+++ domino.py
@@ -2,7 +2,7 @@ class domino():
     def __init__(self, dots1, dots2):
         self.dots1 = dots1
         self.dots2 = dots2
-        self.dots = [dots1, dots2]
+        self.dots = (dots1, dots2)
         self.value = dots1 + dots2
 
     def __eq__(self, other):
